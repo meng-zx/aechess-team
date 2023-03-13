77a9773901d684132e05c7090263ea306770181a
Xingran Shen <xrshen@sjtu.edu.cn>
2023-03-12 20:17:27 -0400

    3.2 implemented

--- GomokuBackend/app/views/ingame.py
+++ GomokuBackend/app/views/ingame.py
@@ -197,5 +197,31 @@ def sendpiece(request):
 @csrf_exempt
 @transaction.atomic
 def clearrecords(request):
+    if request.method != 'POST':
+        return HttpResponse(status=404)
+
+    json_data = json.loads(request.body)
     response = {}
+    userid = json_data['userid']
+
+    cursor = connection.cursor()
+    cursor.execute('DELETE FROM match_queue '
+                   'WHERE userid = %s;', (userid, ))
+
+    cursor.execute('SELECT userid, opponentid FROM game_info '
+                   'WHERE userid = %s OR opponentid = %s;', (userid, userid))
+    result = cursor.fetchone()
+
+    if result is not None:
+        opponentid = result[0] + result[1] - userid
+        if opponentid == -1:
+            cursor.execute('DELETE FROM game_info '
+                           'WHERE userid = %s OR opponentid = %s;', (userid, userid))
+        elif userid == result[0]:
+            cursor.execute('UPDATE game_info SET userid = -1 '
+                       'WHERE userid = %s', (userid, ))
+        else:
+            cursor.execute('UPDATE game_info SET opponentid = -1 '
+                       'WHERE opponentid = %s', (userid, ))
+    response['status'] = 'cleared'
     return JsonResponse(response)
